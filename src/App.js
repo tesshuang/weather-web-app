2da00c83324efd4b13e51b2604a7098368c51d09
Tess Huang <tesshuangxj@yahoo.com>
2021-03-21 14:46:04 -0700

    Add getWeather func and related variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Weather from './components/weather/Weather';
+
 
 function App() {
+  const [city, setCity] = useState('Vancouver');
+  const [notFoundMsg, setNotFoundMsg] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() =>{
+    (async function fetchintialData() {
+      await getWeatherData();
+    })();
+  }, []);
+
+  const getWeatherData = async() => {
+    try {
+      const result = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_WEATHER_APP_API_KEY}&units=metric`);
+      const data = await result.json();
+
+      if (data.cod === '200') {
+        console.log(data);
+        const filtered = data.list.filter((ele, index) => index%8 === 0);
+        setWeather(filtered);
+
+      } else if (data.cod === '404') {
+        setNotFoundMsg(data.message);
+      }
+      
+    } catch(e) {
+      console.error(e);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Weather weather={weather} notFoundMsg={notFoundMsg}/>
+      </main>
     </div>
   );
 }
